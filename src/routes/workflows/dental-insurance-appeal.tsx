2bc38c12eaf1188f4f117057d7d4368e241cbbc6
mycomind4-arch <mycomind4@gmail.com>
2026-08-21 03:17:47 -0700

    feat(appeal-mail): route dental insurance appeal to new workspace

---
+++ src/routes/workflows/dental-insurance-appeal.tsx
@@ -0,0 +1,5 @@
+import { DentalInsuranceAppealWorkspace } from "@/components/workflow/dental-insurance-appeal-workspace";
+
+export default function DentalInsuranceAppealRoute() {
+  return <DentalInsuranceAppealWorkspace />;
+}
